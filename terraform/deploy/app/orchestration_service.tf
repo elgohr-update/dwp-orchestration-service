9c686ab04a75799d51a1222be0fe502e8d3f5588
MattToczek <52243130+MattToczek@users.noreply.github.com>
2020-04-29 16:57:38 +0100

    Dw 3859 template iam role for jupyterhub (#40)
    * adding bucket arn to outputs from tf and adding template policy json
    
    * adding kms as output and kms.arn as env variable
    
    * adding new line at end of json file

--- terraform/deploy/app/orchestration_service.tf
+++ terraform/deploy/app/orchestration_service.tf
@@ -54,6 +54,14 @@ module "ecs-fargate-task-definition" {
     {
       name  = "orchestrationService.jupyterhub_bucket"
       value = module.jupyter_s3_storage.jupyterhub_bucket.id
+    },
+    {
+      name  = "orchestrationService.jupyterhub_kms_arn"
+      value = module.jupyter_s3_storage.jupyterhub_kms.arn
+    },
+    {
+      name  = "orchestrationService.jupyterhub_bucket_arn"
+      value = module.jupyter_s3_storage.jupyterhub_bucket.arn
     }
   ]
 }
--- terraform/modules/jupyter-s3-storage/outputs.tf
+++ terraform/modules/jupyter-s3-storage/outputs.tf
@@ -2,3 +2,8 @@ output "jupyterhub_bucket" {
   description = "The name of the JupyterHub storage bucket"
   value       = aws_s3_bucket.jupyter_storage
 }
+
+output "jupyterhub_kms" {
+  description = "The name of the JupyterHub storage bucket"
+  value       = aws_kms_key.jupyter_bucket_master_key
+}
