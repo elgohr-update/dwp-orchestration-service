504b7efd4563b8f2c2cc74abd8120d47b5b35965
caseyr232 <2818278+caseyr232@users.noreply.github.com>
2020-05-29 15:45:51 +0100

    DW-4179 Apply (#93)

--- terraform/modules/fargate-service/acm.tf
+++ terraform/modules/fargate-service/acm.tf
@@ -12,3 +12,20 @@ resource "aws_acm_certificate" "orchestration-cert" {
 
   tags = merge(var.common_tags, { Name = "${var.name_prefix}-lb" })
 }
+
+//can be removed after apply
+
+resource "aws_acm_certificate" "cert" {
+  domain_name               = local.fqdn
+  certificate_authority_arn = var.cert_authority_arn
+
+  options {
+    certificate_transparency_logging_preference = "DISABLED"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(var.common_tags, { Name = "${var.name_prefix}-lb" })
+}
