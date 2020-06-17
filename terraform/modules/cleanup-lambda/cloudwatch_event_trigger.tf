2bc81911c80c861cd47da2655899a9082f92710e
draagc-iw <55280269+draagc-iw@users.noreply.github.com>
2020-06-17 15:14:38 +0100

    Refactor deprecated interpolation syntax (#114)

--- terraform/modules/cleanup-lambda/cloudwatch_event_trigger.tf
+++ terraform/modules/cleanup-lambda/cloudwatch_event_trigger.tf
@@ -5,17 +5,17 @@ resource "aws_cloudwatch_event_rule" "fire_at_three_seventeen" {
 }
 
 resource "aws_cloudwatch_event_target" "cleanup_lambda_target" {
-  rule      = "${aws_cloudwatch_event_rule.fire_at_three_seventeen.name}"
+  rule      = aws_cloudwatch_event_rule.fire_at_three_seventeen.name
   target_id = "lambda"
-  arn       = "${aws_lambda_function.cleanup_lambda.arn}"
+  arn       = aws_lambda_function.cleanup_lambda.arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_cleanup_lambda" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.cleanup_lambda.function_name}"
+  function_name = aws_lambda_function.cleanup_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.fire_at_three_seventeen.arn}"
+  source_arn    = aws_cloudwatch_event_rule.fire_at_three_seventeen.arn
 }
 
 resource "aws_cloudwatch_log_group" "cleanup_lambda_logs" {
