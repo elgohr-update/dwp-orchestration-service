6e4eb0493849f7d1366a0dacda92d443c53d0638
Liam Rae-McLauchlan <45818099+liamraemclauchlan@users.noreply.github.com>
2020-08-05 17:17:57 +0100

    Revert cloudwatch event rule to 3:17am (#154)
    * Reduce ASG to min 0, and move user tear down to midnight

--- terraform/modules/cleanup-lambda/cloudwatch_event_trigger.tf
+++ terraform/modules/cleanup-lambda/cloudwatch_event_trigger.tf
@@ -1,11 +1,11 @@
-resource "aws_cloudwatch_event_rule" "fire_at_midnight" {
+resource "aws_cloudwatch_event_rule" "fire_at_three_seventeen" {
   name                = "between-three-and-four"
   description         = "Fires every day at 03:17am"
-  schedule_expression = "cron(17 0 * * ? *)"
+  schedule_expression = "cron(17 3 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "cleanup_lambda_target" {
-  rule      = aws_cloudwatch_event_rule.fire_at_midnight.name
+  rule      = aws_cloudwatch_event_rule.fire_at_three_seventeen.name
   target_id = "lambda"
   arn       = aws_lambda_function.cleanup_lambda.arn
 }
@@ -15,7 +15,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_cleanup_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.cleanup_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.fire_at_midnight.arn
+  source_arn    = aws_cloudwatch_event_rule.fire_at_three_seventeen.arn
 }
 
 resource "aws_cloudwatch_log_group" "cleanup_lambda_logs" {
