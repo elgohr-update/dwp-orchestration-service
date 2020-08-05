4573eca3f9c77c56ae7d0b264af4e19f5a6dccc1
Liam Rae-McLauchlan <45818099+liamraemclauchlan@users.noreply.github.com>
2020-08-05 17:44:24 +0100

    [DW-4699] Trigger at midnight (#156)

--- terraform/modules/cleanup-lambda/cloudwatch_event_trigger.tf
+++ terraform/modules/cleanup-lambda/cloudwatch_event_trigger.tf
@@ -1,13 +1,14 @@
-resource "aws_cloudwatch_event_rule" "fire_at_three_seventeen" {
-  name                = "between-three-and-four"
+resource "aws_cloudwatch_event_rule" "fire_at_midnight" {
+  name                = "fire_at_midnightr"
   description         = "Fires every day at 03:17am"
-  schedule_expression = "cron(17 3 * * ? *)"
+  schedule_expression = "cron(11 0 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "cleanup_lambda_target" {
-  rule      = aws_cloudwatch_event_rule.fire_at_three_seventeen.name
+  rule      = aws_cloudwatch_event_rule.fire_at_midnight.name
   target_id = "lambda"
   arn       = aws_lambda_function.cleanup_lambda.arn
+  depends_on = [aws_cloudwatch_event_rule.fire_at_midnight]
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_cleanup_lambda" {
@@ -15,7 +16,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_cleanup_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.cleanup_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.fire_at_three_seventeen.arn
+  source_arn    = aws_cloudwatch_event_rule.fire_at_midnight.arn
 }
 
 resource "aws_cloudwatch_log_group" "cleanup_lambda_logs" {
