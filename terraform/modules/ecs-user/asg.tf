77a2243a299cdf4f14d4af14ed74f837f61ca3ec
Liam Rae-McLauchlan <45818099+liamraemclauchlan@users.noreply.github.com>
2020-08-05 13:07:07 +0100

    Revert "Add taint job for User Host ASG (#147)" (#149)
    This reverts commit b9c2943b738967bbd45014f21812923560c08204.

--- terraform/modules/ecs-user/asg.tf
+++ terraform/modules/ecs-user/asg.tf
@@ -21,6 +21,10 @@ resource "aws_autoscaling_group" "user_host" {
       propagate_at_launch = true
     }
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 data "template_cloudinit_config" "ecs_config" {
