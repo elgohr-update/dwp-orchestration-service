67968de4756d3f733abfe9d32075bf987b2169c3
draagc-iw <55280269+draagc-iw@users.noreply.github.com>
2021-03-01 16:38:54 +0000

    DW-5737 Add cycle old instances job (#207)
    * Add cycle old instances job
    
    * Add TF_WORKSPACE to terraform output

--- terraform/modules/ecs-user/outputs.tf
+++ terraform/modules/ecs-user/outputs.tf
@@ -3,5 +3,6 @@ output "outputs" {
     ecs_cluster              = aws_ecs_cluster.user_host
     security_group_id        = aws_security_group.user_host.id
     user_container_log_group = aws_cloudwatch_log_group.user_container_log_group.name
+    asg                      = aws_autoscaling_group.user_host
   }
 }
