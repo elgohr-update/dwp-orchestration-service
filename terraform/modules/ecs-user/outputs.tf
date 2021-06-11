6ff68db1ac2a5a213d180ff5ad022e0f2b990cf1
steveburton4 <Steven.burton@hotmail.co.uk>
2021-06-11 14:01:22 +0100

    Fix TF13 output and remove unused ones (#244)
    * Fix TF13 output and remove unused ones

--- terraform/modules/ecs-user/outputs.tf
+++ terraform/modules/ecs-user/outputs.tf
@@ -1,8 +1,10 @@
 output "outputs" {
   value = {
-    ecs_cluster              = aws_ecs_cluster.user_host
-    security_group_id        = aws_security_group.user_host.id
+    ecs_cluster = {
+      arn  = aws_ecs_cluster.user_host.arn
+      name = aws_ecs_cluster.user_host.name
+    }
     user_container_log_group = aws_cloudwatch_log_group.user_container_log_group.name
-    asg                      = aws_autoscaling_group.user_host
+    security_group_id        = aws_security_group.user_host.id
   }
 }
