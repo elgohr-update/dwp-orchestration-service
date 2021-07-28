7bfb593b1c6688022563c7637a80e230864374bb
caldwelltom <76474731+caldwelltom@users.noreply.github.com>
2021-07-28 16:31:32 +0100

    Dw 6830 remove rodbcdbi package (#272)
    * update rstudio image in dev
    
    * use most recent rstudiooss version in all envs
    
    Co-authored-by: thomasstock-caldwell <thomas.stock-caldwell@engineering.digital.dwp.gov.uk>

--- terraform/deploy/app/variables.tf
+++ terraform/deploy/app/variables.tf
@@ -301,7 +301,7 @@ variable "component_tags" {
     }
     qa = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.48"
+      rstudio_oss     = "0.0.49"
       jupyter_hub     = "0.0.66"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
@@ -310,7 +310,7 @@ variable "component_tags" {
     }
     integration = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.48"
+      rstudio_oss     = "0.0.49"
       jupyter_hub     = "0.0.66"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
@@ -319,7 +319,7 @@ variable "component_tags" {
     }
     preprod = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.48"
+      rstudio_oss     = "0.0.49"
       jupyter_hub     = "0.0.66"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
@@ -328,7 +328,7 @@ variable "component_tags" {
     }
     production = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.48"
+      rstudio_oss     = "0.0.49"
       jupyter_hub     = "0.0.66"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
