495fca6a0242375a62ff4db685ab51a88fb38dc3
Ian Utley <ianutley@digital.uc.dwp.gov.uk>
2021-07-29 14:43:24 +0100

    Increase user cluster size (#274)

--- terraform/modules/ecs-user/ecs.tf
+++ terraform/modules/ecs-user/ecs.tf
@@ -20,8 +20,8 @@ resource "aws_ecs_capacity_provider" "user_host" {
 
     managed_scaling {
       status                    = "ENABLED"
-      maximum_scaling_step_size = 10
-      minimum_scaling_step_size = 1
+      maximum_scaling_step_size = 40
+      minimum_scaling_step_size = 2
       target_capacity           = 80
     }
 
