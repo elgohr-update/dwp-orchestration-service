361e2fc3ac03d1ec9757e69d4ac1a488dd32ad6a
Sumit Bharadia <36968381+03ce007@users.noreply.github.com>
2021-09-03 08:35:12 +0100

    upgrades RStudio version in dev to test the new pkgs installation (#289)
    Co-authored-by: sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>

--- terraform/deploy/app/variables.tf
+++ terraform/deploy/app/variables.tf
@@ -292,7 +292,7 @@ variable "component_tags" {
   default = {
     development = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.49"
+      rstudio_oss     = "0.0.50"
       jupyter_hub     = "0.0.67"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
