98027a69c73bcd6c1f20f34e7112c35ad349c1bc
tobywalls <60394163+tobywalls@users.noreply.github.com>
2021-10-29 15:23:07 +0100

    Bump version of rstudio image in envs up to preprod (#329)

--- terraform/deploy/app/variables.tf
+++ terraform/deploy/app/variables.tf
@@ -292,7 +292,7 @@ variable "component_tags" {
   default = {
     development = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.61"
+      rstudio_oss     = "0.0.62"
       jupyter_hub     = "0.0.76"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
@@ -301,7 +301,7 @@ variable "component_tags" {
     }
     qa = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.61"
+      rstudio_oss     = "0.0.62"
       jupyter_hub     = "0.0.76"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
@@ -310,7 +310,7 @@ variable "component_tags" {
     }
     integration = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.61"
+      rstudio_oss     = "0.0.62"
       jupyter_hub     = "0.0.76"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
@@ -319,7 +319,7 @@ variable "component_tags" {
     }
     preprod = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.61"
+      rstudio_oss     = "0.0.62"
       jupyter_hub     = "0.0.76"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
